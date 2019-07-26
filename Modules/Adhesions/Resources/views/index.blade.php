80fb3d68e0c42a05e42930993505070f3be4a41a
fmseaPierreVigouroux <pierre.vigouroux@fms-ea.com>
2019-07-26 15:12:00 +0200

    Revert "Base skeleton in developpement"
    This reverts commit 61bd85c0984083ee5b88d036ab4a65f7c2a9c4c6.

--- Modules/Adhesions/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('adhesions::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('adhesions.name') !!}
-    </p>
-@stop
